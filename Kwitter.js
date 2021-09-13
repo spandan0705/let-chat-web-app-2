6b40f4272565ed37442fb39409b0212441ea63f7
spandan0705 <85344474+spandan0705@users.noreply.github.com>
2021-09-13 10:06:44 +0530

    Add files via upload

---
+++ Kwitter.js
@@ -0,0 +1,7 @@
+function addUser() 
+{
+    user_name=document.getElementById("user_name").value;
+
+    localStorage.setItem("üser_name", user_name);
+    window.location="kwitter_room.html";
+}
---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA3lEf2qf60C35Q1FByj9xQxk6Q-WtxshQ",
+    authDomain: "let-s-chat-e947d.firebaseapp.com",
+    databaseURL: "https://let-s-chat-e947d-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-e947d",
+    storageBucket: "let-s-chat-e947d.appspot.com",
+    messagingSenderId: "836415319153",
+    appId: "1:836415319153:web:a4c2085a4748dd7fe9ffd0",
+    measurementId: "G-87SZ8L1TVQ"
+  };
